97112a6235f210434656ea1693637cb4ff111684
ADRIAN SYPOS - Student <adrian00935@gmail.com>
2016-10-03 10:51:50 +0100

    Flask, 4 - adding comments

--- hello.py
+++ hello.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
-	
+
+##when accessing  127.0.0.1:8000/name/SOMETHING
+##returns Your name is SOMETHING	
 @app.route('/name/<user>')
 def name(user):
     return 'Your name is %s' % user
